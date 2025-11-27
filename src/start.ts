72252e5f00fd65a66a06a36f6514627b329393ab
qiucw <doitmyway@qq.com>
2025-11-27 14:10:24 +0800

    init add

---
+++ src/start.ts
@@ -0,0 +1,6 @@
+
+let isDevelopment = (process.env.NODE_ENV === 'development');
+console.log('isDevelopment ? ' + (isDevelopment ? 'yes' : 'no'));
+
+let workingDir = process.cwd();
+console.log('workingDir: ' + workingDir);
