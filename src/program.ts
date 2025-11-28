c447f2acad8cf56e06d4f93779fd22f80518878f
qiucw <doitmyway@qq.com>
2025-11-28 20:24:48 +0800

    no more node!

--- src/program.ts
+++
@@ -1,8 +0,0 @@
-function main() {
-    let isDevelopment = (process.env.NODE_ENV === 'development');
-    console.log('isDevelopment ? ' + (isDevelopment ? 'yes' : 'no'));
-
-    let workingDir = process.cwd();
-    console.log('workingDir: ' + workingDir);
-}
-main();
