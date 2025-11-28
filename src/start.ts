aaa5dbcde7242ffa81d19965d80adf23ef2dfb90
qiucw <doitmyway@qq.com>
2025-11-28 20:24:22 +0800

    commit ts, will delete all

--- src/start.ts
+++
@@ -1,6 +0,0 @@
-
-let isDevelopment = (process.env.NODE_ENV === 'development');
-console.log('isDevelopment ? ' + (isDevelopment ? 'yes' : 'no'));
-
-let workingDir = process.cwd();
-console.log('workingDir: ' + workingDir);
